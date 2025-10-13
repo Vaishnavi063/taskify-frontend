3e343bce0e1df2a89c468dc23f1f564d61cf486c
krishmungase <mungasekrishna8@gmail.com>
2025-10-13 17:26:54 +0530

    feat: initial setup

---
+++ src/configs/site-configs.ts
@@ -0,0 +1,9 @@
+const siteConfigs = {
+  name: "Taskify",
+  links: {
+    github: "https://github.com/code-mitra-yt/bl-sheet-client",
+    linkedin: "https://www.linkedin.com/in/rushikesh-mungse",
+  },
+};
+
+export default siteConfigs;
